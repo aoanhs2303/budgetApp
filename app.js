5215a15a9c5f928e6dc996eca1c1ad4c9a51f3e2
AoAnhs2303 <trannhulucs2303@gmail.com>
2017-10-27 23:17:44 +0700

    complete

--- app.js
+++ app.js
@@ -277,8 +277,25 @@ var UIController = (function() {
             month = now.getMonth();
 
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+   
+        },
 
-        
+        changeType: function() {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ', ' +
+                DOMstrings.inputDescription + ', ' +
+                DOMstrings.inputValue
+            ); //phai dung forech doi duyet tung input
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i)
+                }
+            };
+            nodeListForEach(fields, function(current) {
+                current.classList.toggle('red-focus');
+            })
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
         getDOMstrings: function() {
@@ -301,6 +318,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changeType)
+    
     };
 
     var updateBudget = function() {
