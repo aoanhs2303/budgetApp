c2673f14aaef6ea5348d09627a705506f9921532
AoAnhs2303 <trannhulucs2303@gmail.com>
2017-10-27 12:14:21 +0700

    Bai 66

--- app.js
+++ app.js
@@ -21,6 +21,33 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            //[1 2 4 6] next ID = 7
+            //ID = last ID + 1
+            //Create new ID   
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            //Create new Item
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            data.allItems[type].push(newItem); // chu y dung [] de chon phan tu trong object
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
     }
 })();
 
@@ -62,10 +89,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
     
     var ctrlAddItem = function() {
+        var input, newItem;
+        
         //1. Get the field input data
-        var input = UICtrl.getinput();
-        console.log(input);
+        input = UICtrl.getinput();
+        console.log(input.type);
         //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3. Add the item to the UI
 
